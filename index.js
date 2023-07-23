77db6c21beccd105610ea5d31310a3a33114f51e
hadiadjd <celestialgeek21@gmail.com>
2023-07-23 15:33:18 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Follow along with the examples here
+/*function sayHello() {
+  console.log("Hello!");
+}
+sayHello(); */
+
+//Testing step by step
+
+/*function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
+
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+sayHelloToLiz();
+
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+sayHelloToSamip();*/
+
+//Retesting following the README file
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+
+
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+
+
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+/*function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+sayHelloTo("Jane"); // "Hello, Jane!"
+*/
+
+
+//testing another step from README
+
+
+
+/*function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`);
+  return `${greeting}, ${firstName}!`;
+}
+say("Howdy", "partner");*/
+
+//finally, testing on my own name 
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`);
+  return `${greeting}, ${firstName}!`;
+}
+say("Hola", "Hadia");
